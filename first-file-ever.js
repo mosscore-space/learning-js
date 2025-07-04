e238faaa46bc650a2f8f9d25c0d97ee6f76e2b04
moss <mosscore.dev@gmail.com>
2025-07-04 23:54:41 +0500

    initial commit

---
+++ first-file-ever.js
@@ -0,0 +1,41 @@
+// yo yo gonna intialise git repo now
+// ok i successfully did it
+// 1. git init
+// 2. the repo was in /home/ma name/.git - did not know that and then did git add .
+// omg i feel like the whole freaking os was cloned
+// 3. git reset - git status - should see something like nothing added to commit
+// 4 cd to the correct folder
+// 5. git add . - boo shaka laka im back in business
+// 6. git commit -am "initial commit"
+// 7. wait not yet
+// 8.git remote add origin (add the repo link here. ofc remove the brackets. its not needed)
+// 9. git commit -am "initial commit"
+// 10. git push origin master
+
+//start
+// js does not need to define a main fuction like c, c++, java, etc. You can directly write your code.
+
+console.log("Hello World!");
+// consol.log() serves as a method on printing values to the consol
+// the " " are indicating the text as plain text and not as a function
+// the semicolon ; shows the ending of a command
+
+// <-- these are single line comments
+// console.log("test 1")
+// as you can see the command is not excecuted
+// you can add it to the end of a command as well
+console.log("test 2"); // like this
+
+// then there is multi-line comments
+// you have to write /* at the start and */ at the end
+/*
+console.log("test 3");
+console.log("test 3.1");
+console.log("test 3.2");
+console.log("test 3.3");
+console.log("test 3.4");
+console.log("test 3.5");
+*/
+// like that
+// also can use it inside code
+console.log("test 4",/*"test 5", */"tests 6");
