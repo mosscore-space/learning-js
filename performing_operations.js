5caf927a44ad76516eaee031e5ddcc1ebd07e36a
moss <mosscore.dev@gmail.com>
2025-07-11 02:30:08 +0500

    operations and operators

--- performing_operations.js
+++ performing_operations.js
@@ -82,3 +82,130 @@ console.log(((25 + 7) * 2) **3); // culculated as 25 + 7 = 32, 32 * 2 = 64, 64^3
 
 // NOTE:the parenthesis or the brackets () have the highest priorities. from the most inner one followed by the shallower one and so on
 
+// Assignment Operators_____
+
+// assignment apparantly can streamline the code, making it more readable and elegant
+// js has several assignment oprators
+
+// additional assignment ( += )
+// subtraction assignment ( -= )
+// multiplication assignment ( *= )
+// division assignment (/=)
+// reminder (modulo) assignment ( %= )
+// exponentiation assignment ( **= )
+
+// as mentioned before, assignment operators are used to enchance code readability
+// consider the following example
+let c = 17;
+c = c + 5;
+
+console.log(c);
+
+// but we can write the code more beautiful like this
+
+let d = 17;
+d += 5;
+
+console.log(d);
+
+// the expression a += 5 is yields the same result as a = a + 5
+
+let e = 5;
+let f = 6;
+let g = 7;
+let h = 8;
+let i = 9;
+let j = 10;
+
+e += j;
+f *= i;
+g /= h;
+h %= j;
+i **= e;
+j /= f;
+
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
+console.log(i);
+console.log(j);
+
+// well whatever, you get the point
+
+// Increment and Decrement Operators_____
+
+// increment and Decrement are operators that increase or decrease a varuiable by 1
+
+// Increment_____
+
+// its performed using ++ operator
+
+let k = 1;
+
+k++; // seams like no space is given
+
+console.log(k);
+
+// Decrement
+
+// as you have already guessed it, its performed using -- operator
+
+k--;
+k--; // reduces 2 times so now the number should be zero
+
+console.log(k); // allright 
+
+// NOTE: the expression k++ is same as k = k +1 or k += 1
+// vice verser
+
+// Comparison Operators_____
+
+// comparison operators are used to define a condition, perform checks and make decisions. these operators return a boolean ( true or false )
+
+// equals to ( == )
+// srict equals to ( === )
+// not equals to ( != )
+// strict not equals to ( !== )
+// greater than ( > )
+// greater than or equals to ( >= )
+// less than ( < )
+// less than or equals to ( <= )
+
+// Equal and Not Equal_____
+
+console.log("Equal to:");
+console.log("5 == 5 is", 5 == 5); // this is correct
+console.log("5 == 4 is", 5 == 4); // not correct
+console.log("5 == 4.99 is", 5 == 4.99); // not correct
+            
+console.log("Not equal to:");
+console.log("5 != 5 is", 5 != 5); // not correct
+console.log("5 != 4 is", 5 != 4); // is correct
+console.log("5 != 4.99 is", 5 != 4.99); // is correct
+
+// Strict Comparison
+
+console.log("Equal to:");
+console.log("5 === 5 is", 5 === 5); // this is correct
+console.log("5 === 4 is", 5 === 4); // not correct
+console.log("5 === 4.99 is", 5 === 4.99); // not correct
+            
+console.log("Not equal to:");
+console.log("5 !== 5 is", 5 !== 5); // not correct
+console.log("5 !== 4 is", 5 !== 4); // is correct
+console.log("5 !== 4.99 is", 5 !== 4.99); // is correct
+
+// bruh whats the use of diferentiating it
+// why have strict and unstrict
+
+console.log(null == undefined);   // true
+console.log(null === undefined);  // false
+
+// whaat
+
+// so the == operator performs type coercion. it convers one operand so their types match, then conpares value.
+// the === operator skips coercion and compares both type and value directly
+
+// so what that means is technically saying null does mean undefined but === compares the word for word, not the meaning?
+// thats what i understood
